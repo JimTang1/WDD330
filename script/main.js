6f92cff033dcfd80fc2d380299938efe641dc236
JimTang <after062111@byui.edu>
2021-01-23 18:40:38 -0700

    update week03 notes

--- script/main.js
+++ script/main.js
@@ -4,7 +4,13 @@ let ol = document.querySelector('ol');
 const links = [
 {
     label: "Week 1",
-    url: "week1/index.html"
+    url: "week1/index.html",
+
+    label2: "Week 2",
+    url2: "week2/index.html",
+
+    label3: "Week 3",
+    url3: "week3/index.html"
 }
 
 ];
@@ -12,6 +18,9 @@ const links = [
 links.forEach(
     link =>{
         ol.innerHTML +=
-        `<li><a href = "${link.url}">${link.label}`
+        `<li><a href = "${link.url}">${link.label}</li>
+        <li><a href = "${link.url2}">${link.label2}</li>
+        <li><a href = "${link.url3}">${link.label3}</li>
+        `
     }
 );
